d9c04b2b7736c78fbc2e0461f2e62d76af3270ef
Siddhant Tandon <cmxiv914@gmail.com>
2024-07-13 14:30:02 +0200

    Step 1: No types

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "dev.tandosid.playground.kotlin"
@@ -10,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,89 @@
 package dev.tandosid.playground.kotlin
 
+import dev.tandosid.playground.kotlin.EngineType.FOUR_STROKE
+import dev.tandosid.playground.kotlin.EngineType.TWO_STROKE
+import dev.tandosid.playground.kotlin.VehicleType.BIKE
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import java.time.Clock
+import java.time.Year
+import kotlin.random.Random
+
 fun main() {
-    println("Hello World!")
+    println("Hello, Polymorphic Serialization!")
+
+    val twoStrokeBikeJson = createRandomVehicleResponse(BIKE, TWO_STROKE)
+    println(twoStrokeBikeJson)
+
+    val fourStrokeBikeJson = createRandomVehicleResponse(BIKE, FOUR_STROKE)
+    println(fourStrokeBikeJson)
+}
+
+fun createRandomVehicleResponse(vehicleType: VehicleType, engineType: EngineType): String {
+    val engine = when(engineType) {
+        TWO_STROKE -> TwoStroke
+        FOUR_STROKE -> FourStroke
+    }
+
+    val vehicle = when (vehicleType) {
+        BIKE -> Bike(
+            engine = engine,
+            mileage = Random.nextLong(10, 1000),
+            yearOfManufacturing = Year.of(Random.nextInt(1999, Year.now(Clock.systemUTC()).value))
+        )
+    }
+
+    return Json.encodeToString(vehicle)
+}
+
+enum class EngineType {
+    TWO_STROKE,
+    FOUR_STROKE
+}
+
+@Serializable
+sealed class Engine(val stokes: Int)
+
+@Serializable
+@SerialName("two-stroke")
+data object TwoStroke: Engine(2)
+
+@Serializable
+@SerialName("four-stroke")
+data object FourStroke: Engine(4)
+
+
+enum class VehicleType {
+    BIKE
+}
+
+@Serializable
+sealed interface Vehicle {
+    val engine: Engine
+}
+
+@Serializable
+@SerialName("bike")
+data class Bike(
+    val mileage: Long,
+    override val engine: Engine,
+    @Serializable(YearSerializer::class)
+    val yearOfManufacturing: Year,
+): Vehicle
+
+class YearSerializer: KSerializer<Year> {
+    override val descriptor = PrimitiveSerialDescriptor("year", PrimitiveKind.INT)
+
+    override fun deserialize(decoder: Decoder): Year = Year.of(decoder.decodeInt())
+
+    override fun serialize(encoder: Encoder, value: Year) {
+        encoder.encodeInt(value.value)
+    }
 }
